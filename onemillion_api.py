ddfc0cf71f491e47514ef8d1ee1ab41fbf785132
Floyd Hightower <floyd.hightower27@gmail.com>
2018-01-06 07:08:46 -0400

    Renaming files and improving structure and UI

---
+++ onemillion_api.py
@@ -0,0 +1,30 @@
+import onemillion
+from flask import Flask, render_template, redirect, request, flash, url_for
+
+app = Flask(__name__)
+app.secret_key = 'abc'
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/onemillion', methods=['POST'])
+def get_onemillion():
+    if request.form['domain']:
+        domain = request.form['domain']
+        return redirect(url_for('show_onemillion', domain=domain))
+    else:
+        flash('Please enter a domain.', 'error')
+        return redirect(url_for('index'))
+
+
+@app.route('/onemillion/<domain>')
+def show_onemillion(domain):
+    o = onemillion.OneMillion(cache_location='./cache', update=False)
+    return "{}".format(o.domain_in_million(domain))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
